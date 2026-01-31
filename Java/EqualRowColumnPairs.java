01f48cf49e789260058fb1ebdcaac261da6b49d4
Sandeepkumar-S-18 <tech.sandeep18@gmail.com>
2026-01-31 18:58:20 +0530

    Java Q-2

--- Java/EqualRowColumnPairs.java
+++ Java/EqualRowColumnPairs.java
@@ -1,5 +1,41 @@
 import java.util.Scanner;
 
 class EqualRowColumnPairs {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int arr[][] = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println(equalRowColumnPairs(n, arr));
+	}
 	
+	public static int equalRowColumnPairs(int n, int arr[][]) {
+		int pair = 0;
+		
+		for(int r = 0; r < n; r++) {
+			for(int c = 0; c < n; c++) {
+				boolean isMatch = true;
+				
+				for(int k = 0; k < n; k++) {
+					if(arr[r][k] != arr[k][c]) {
+						isMatch = false;
+						break;
+					}
+				}
+				
+				if(isMatch) {
+					pair++;
+				}
+			}
+		}
+		
+		return pair;
+	}
 }
