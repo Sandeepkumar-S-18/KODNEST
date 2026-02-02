330614361c7e3185a3b3245bed8aae76e4fdc7de
Sandeepkumar-S-18 <tech.sandeep18@gmail.com>
2026-02-02 17:07:53 +0530

    Java Ternary operator and Variables

---
+++ Java/classPractice/JavaBasics/src/com/JavaBasics/Operators/TernaryOperator.java
@@ -0,0 +1,19 @@
+package com.JavaBasics.Operators;
+
+public class TernaryOperator {
+	public static void main(String[] args) {
+		/*
+		var a = 12;
+		var b = 2;
+		
+		var result = (a > b) ? "A is Greater." : "B is Greater";
+		
+		System.out.println(result);
+		*/
+		
+		var age = 18;
+		var result = age > 18 ? "The person is eligible for voting." : "The person is not eligible for voting.";
+		
+		System.out.println(result);
+	}
+}
---
+++ Java/classPractice/JavaBasics/src/com/JavaBasics/Operators/Variable.java
@@ -0,0 +1,25 @@
+package com.JavaBasics.Operators;
+
+public class Variable {
+	public static void main(String[] args) {
+		var ageVar = 23;
+		var nameVar = "Sandeep";
+		var genderVar = 'M';
+		var isMarriedVar = false;
+		
+		int ageDt = 23;
+		String nameDt = "Sandeep";
+		char genderDt = 'M';
+		boolean isMarriedDt = false;
+		
+		System.out.println(ageVar);
+		System.out.println(nameVar);
+		System.out.println(genderVar);
+		System.out.println(isMarriedVar);
+		
+		System.out.println(ageDt);
+		System.out.println(nameDt);
+		System.out.println(genderDt);
+		System.out.println(isMarriedDt);
+	}
+}
